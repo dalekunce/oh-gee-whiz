c326b1b32f85c43d5364fb67d17d2d1eed1eec08
dale@normalhabit.com <dale@normalhabit.com>
2018-04-28 15:38:49 -0700

    status now has db connection status, adding sample config.json

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
+/* jshint esversion: 6 */
+const config = require('./config.json')
 const restify = require('restify')
 const tokml = require('tokml')
+const testJSON = require('./data/test.json')
 
-var testJSON = require('./data/test.json')
+
+/** SQL connection **/
+const Connection = require('tedious').Connection
+const Request = require('tedious').Request
+const TYPES = require('tedious').TYPES
+
+// Attempt to connect to database
+const result = {}
+const connection = new Connection(config)
+connection.on('connect', function (err) {
+  if (err) {
+    result.dbconnection = err
+    console.log('ERROR DB')
+  } else {
+    result.dbstatus = 'Connected'
+    console.log('Connected to DB')
+  }
+})
 
 /** Utilities **/
 
 function statusUpdate (req, res, next) {
-  res.send('All Good!')
-  next()
+  result.serverstatus = 'API reporting for duty'
+
+  res.set({
+    'content-type': 'application/json'
+  })
+  res.send(result)
 }
 
 function getKmlTest (req, res, next) {
@@ -22,13 +46,14 @@ function getKmlTest (req, res, next) {
   next()
 }
 
+/* Turn the server on */
 const server = restify.createServer()
 
 /* Is the server up */
-server.get('/status/', statusUpdate)
+server.get('/status', statusUpdate)
 
 /** Get KML Test **/
-server.get('/kml/test/', getKmlTest)
+server.get('/kml/test', getKmlTest)
 
 server.listen(3000, function () {
   console.log('%s listening at %s', server.name, server.url)
