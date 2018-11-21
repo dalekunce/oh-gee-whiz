398b069c5d6b704a334aac0717026cd804bdccf0
dale@normalhabit.com <dale@normalhabit.com>
2018-11-20 22:07:47 -0800

    fixed geopoint typo

--- cron.js
+++ cron.js
@@ -44,6 +44,7 @@ const getData = function () {
     LinkToWebsite as [properties.LinkToWebsite],
     LinkToOverlay as [properties.LinkToOverlay],
     Participation as [properties.Participation],
+    'Point' as [geometry.type],
     JSON_QUERY
     ( FORMATMESSAGE('[%s,%s]',
     FORMAT(longitude, N'0.##########'),
@@ -65,50 +66,49 @@ const getData = function () {
       combined = JSON.parse(combined)
       resolve(combined)
     })
-  // }).then(function (q) {
-  //   console.log('DESCRIPTING')
-  //   return new Promise((resolve, reject) => {
-  //     _.forEach(q, function (element, i) {
-  //       // set description for later use by tokml
-  //       console.log('feature' + i)
-  //       // let desc = 'this is a test' +
-  //       // ' more testing' + q[i].properties.SiteNo
-  //       console.log(q[i].properties)
-  //       let desc = ''
-  //       // let desc = '<![CDATA[<!DOCTYPE html>' +
-  //       //   '<html xmlns="http://www.w3.org/1999/xhtml" style="width:100%; height: 100%;"><head>' +
-  //       //   '<title>KIMCO Detail | ' + q[i].properties.SiteNo + '</title>' +
-  //       //   '<meta http-equiv="content-type" content="text/html; charset=utf-8"/></head><body><font face="Verdana"><a href="http://kimcorealty.com"><img border="0" width="32px" src="http://gee-server.kimcorealty.com/icons/kimco_2017.png" alt="Kimco Logo" /></a>' +
-  //       //   '<br /><br />' +
-  //       //   '<b>Site No:</b>' + q[i].properties.SiteNo + '<br />' +
-  //       //   '<b>Center Name:</b>' + q[i].properties.CenterName + '<br />' +
-  //       //   '<b>Address:</b>' + q[i].properties.Address + '<br />' +
-  //       //   '<b>Region:</b>' + q[i].properties.Region + '<br />' +
-  //       //   '<b>GLA:</b>' + q[i].properties.GLA + '<br />' +
-  //       //   '<b>Partnership:</b>' + q[i].properties.Partnership + '<br />' +
-  //       //   '<hr /><b>Link to Web Site:</b> <br /><a href="LinkToWebs" target="asset">' + q[i].properties.LinkToWebs + '</a>' +
-  //       //   '<hr /><b>Marketing Brochure:</b> <br /><a href="' + q[i].properties.MarketingBrochure + '" target="asset">' + q[i].properties.MarketingBrochure + '</a>' +
-  //       //   '<hr />' +
-  //       //   '<b>Overlay Site Plan:</b> <a href="' + q[i].properties.LinkToOverlay + '" target="asset">Click to see ' + q[i].properties.SiteNo + ' site plan</a><br />' +
-  //       //   '<hr />' +
-  //       //   '<b>Property Manager:</b> ' + q[i].properties.PropertyManager + '<br />' +
-  //       //   '<b>Leasing Agent:</b> ' + q[i].properties.LeasingAgent + '<br />' +
-  //       //   '<hr /><br />' +
-  //       //   '</font>' +
-  //       //   '</body>' +
-  //       //   '</html>]]>'
-  //
-  //       let description = {
-  //         'Description': desc
-  //       }
-  //
-  //       _.assign(q[i].properties, description)
-  //
-  //       // console.log(q[i])
-  //     })
-  //     console.log(q)
-  //     resolve(q)
-  //   })
+  }).then(function (q) {
+    console.log('DESCRIPTING')
+    return new Promise((resolve, reject) => {
+      _.forEach(q, function (element, i) {
+        // set description for later use by tokml
+        console.log('feature' + i)
+        // let desc = 'this is a test' +
+        // ' more testing' + q[i].properties.SiteNo
+        console.log(q[i].properties)
+        let desc = '<![CDATA[<!DOCTYPE html>' +
+          '<html xmlns="http://www.w3.org/1999/xhtml" style="width:100%; height: 100%;"><head>' +
+          '<title>KIMCO Detail | ' + q[i].properties.SiteNo + '</title>' +
+          '<meta http-equiv="content-type" content="text/html; charset=utf-8"/></head><body><font face="Verdana"><a href="http://kimcorealty.com"><img border="0" width="32px" src="http://gee-server.kimcorealty.com/icons/kimco_2017.png" alt="Kimco Logo" /></a>' +
+          '<br /><br />' +
+          '<b>Site No:</b>' + q[i].properties.SiteNo + '<br />' +
+          '<b>Center Name:</b>' + q[i].properties.CenterName + '<br />' +
+          '<b>Address:</b>' + q[i].properties.Address + '<br />' +
+          '<b>Region:</b>' + q[i].properties.Region + '<br />' +
+          '<b>GLA:</b>' + q[i].properties.GLA + '<br />' +
+          '<b>Partnership:</b>' + q[i].properties.Partnership + '<br />' +
+          '<hr /><b>Link to Web Site:</b> <br /><a href="LinkToWebs" target="asset">' + q[i].properties.LinkToWebs + '</a>' +
+          '<hr /><b>Marketing Brochure:</b> <br /><a href="' + q[i].properties.MarketingBrochure + '" target="asset">' + q[i].properties.MarketingBrochure + '</a>' +
+          '<hr />' +
+          '<b>Overlay Site Plan:</b> <a href="' + q[i].properties.LinkToOverlay + '" target="asset">Click to see ' + q[i].properties.SiteNo + ' site plan</a><br />' +
+          '<hr />' +
+          '<b>Property Manager:</b> ' + q[i].properties.PropertyManager + '<br />' +
+          '<b>Leasing Agent:</b> ' + q[i].properties.LeasingAgent + '<br />' +
+          '<hr /><br />' +
+          '</font>' +
+          '</body>' +
+          '</html>]]>'
+
+        let description = {
+          'Description': desc
+        }
+
+        _.assign(q[i].properties, description)
+
+        // console.log(q[i])
+      })
+      console.log(q)
+      resolve(q)
+    })
   }).then(function (d1) {
     let d2 = JSON.stringify(d1, null, 2)
 
